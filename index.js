07951c84171b34b3e87d54fde2df489b5c25ca54
Sardor <normurodovsardorbek472@gamil.com>
2023-09-17 23:35:44 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// && = va qatiy
+// || yoki qatiy emas
+
+// const isAge = true;
+// const isClose = false;
+// const isChekked = false;
+
+// console.log(!isAge || !isChekked || isClose);
+// console.log(!isAge);
+
+// console.log(2 + 2 * "2" == 6);
